eac0728527cf1f1b40d8d3da181c32d8e0aac95c
unknown <ehsan.hd.1996@gmail.com>
2023-11-09 15:46:17 +0330

    git initialized

---
+++ EventAndDelegates/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EventAndDelegates;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var video = new Video("MyVideo");
+        var encoder = new VideoEncoder();   //publisher
+        var mailService = new MailService(); //subscriber
+
+        encoder.VideoEncoded += mailService.OnVideoEncoded;   //Subscribing the listener
+
+        encoder.Encode(video);
+    }
+}
+
+public class MailService
+{
+    public void OnVideoEncoded(object source, EventArgs e)
+    {
+        Console.WriteLine("Mail Service: Sending An Email ....");
+    }
+}
